b51f970920e0670d97387e84a476a848621a40e6
tomiy <tom.chappaz1@gmail.com>
2019-08-03 22:59:38 +0200

    Added message object

---
+++ chat/message.class.php
@@ -0,0 +1,115 @@
+<?php
+
+namespace CrossbladeBot\Chat;
+
+class Message
+{
+
+    public $raw;
+    public $tags;
+    public $type;
+    public $channel;
+    public $message;
+    public $command;
+    public $from;
+    public $params;
+    public $id;
+
+    public function __construct($string)
+    {
+        $this->raw = trim($string);
+        $this->parse();
+        $this->badges();
+        $this->badgeinfo();
+        $this->emotes();
+    }
+
+    private function parse()
+    {
+        $regex = implode('', [
+            'open' => '/^',
+            'tags' => '(?:@(?P<tags>[^\r\n ]*) +|())',
+            'from' => '(?::(?P<from>[^\r\n ]+) +|())',
+            'type' => '(?P<type>[^\r\n ]+)',
+            'params' => '(?: +(?P<params>[^:\r\n ]+[^\r\n ]*(?: +[^:\r\n ]+[^\r\n ]*)*)|())?',
+            'trailing' => '(?: +:(?P<trailing>[^\r\n]*)| +())?[\r\n]*',
+            'close' => '$/'
+        ]);
+
+        $match = preg_match($regex, $this->raw, $matches);
+
+        if(!$match) return false;
+
+        if($this->raw[0] === '@') {
+            $rawtags = explode(';', $matches['tags']);
+
+            for ($i = 0; $i < sizeof($rawtags); $i++) {
+                $tag = $rawtags[$i];
+                $pair = explode('=', $tag);
+                $this->tags[$pair[0]] = $pair[1];
+            }
+        }
+
+        $this->from = $matches['from'];
+        $this->type = $matches['type'];
+
+        if(!empty($matches['params'])) {
+            $this->params = explode(' ', $matches['params']);
+        }
+
+        if(isset($matches['trailing'])) {
+            $this->params[] = $matches['trailing'];
+        }
+
+        $this->channel = $this->params[0] ?? null;
+        $this->message = $this->params[1] ?? null;
+        $this->id = $this->tags['msg-id'] ?? null;
+    }
+
+    private function badges()
+    {
+        $this->parsetag('badges');
+    }
+
+    private function badgeinfo()
+    {
+        $this->parsetag('badge-info');
+    }
+
+    private function emotes()
+    {
+        $this->parsetag('emotes', '/', ':', ',');
+    }
+
+    private function parsetag($index, $delim1 = ',', $delim2 = '/', $delim3 = null)
+    {
+
+        if (!isset($this->tags[$index])) {
+            return;
+        }
+
+        $raw = $this->tags[$index];
+
+        if ($raw === true) {
+            $this->tags[$index] = null;
+            return;
+        }
+
+        $this->tags[$index] = [];
+
+        if (is_string($raw)) {
+            $spl = explode($delim1, $raw);
+
+            for ($i = 0; $i < sizeof($spl); $i++) {
+                $parts = explode($delim2, $spl[$i]);
+                if ($parts[0]) {
+                    $val = $parts[1];
+                    if ($delim3 && $val) {
+                        $val = explode($delim3, $val);
+                    }
+                    $this->tags[$index][$parts[0]] = $val ?? null;
+                }
+            }
+        }
+    }
+}
--- core/client.class.php
+++ core/client.class.php
@@ -3,6 +3,7 @@
 namespace CrossbladeBot\Core;
 
 use CrossbladeBot\Traits\Configurable;
+use CrossbladeBot\Chat\Message;
 
 class Client extends Configurable
 {
@@ -49,7 +50,292 @@ class Client extends Configurable
             $data = $this->socket->getNext();
 
             if ($data) {
-                print_r($data);
+                $message = new Message($data);
+
+                if (is_null($message->from)) {
+                    switch ($message->type) {
+                        case 'PING':
+                            $lastping = time();
+                            $this->socket->send('PONG');
+                            //pong event
+                            break;
+                        case 'PONG':
+                            $latency = time() - $lastping;
+                            $this->logger->info('Current latency: ' . $latency);
+                            break;
+                        default:
+                            $this->logger->warning('Could not parse message: ' . $message->raw);
+                            break;
+                    }
+                } else if ($message->from === 'tmi.twitch.tv') {
+                    switch ($message->type) {
+                        case '002':
+                        case '003':
+                        case '004':
+                        case '375':
+                        case '376':
+                        case 'CAP':
+                            break;
+                        case '001':
+                            $this->name = $message->params[0];
+                            break;
+                        case '372':
+                            $this->logger->info('Client connected');
+                            //connect event
+                            break;
+                        case 'NOTICE':
+                            switch ($message->id) {
+                                case "subs_on":
+                                    break;
+                                case "subs_off":
+                                    break;
+                                case "emote_only_on":
+                                    break;
+                                case "emote_only_off":
+                                    break;
+                                case "slow_on":
+                                case "slow_off":
+                                    break;
+                                case "followers_on_zero":
+                                case "followers_on":
+                                case "followers_off":
+                                    break;
+                                case "r9k_on":
+                                    break;
+                                case "r9k_off":
+                                    break;
+                                case "room_mods":
+                                    break;
+                                case "no_mods":
+                                    break;
+                                case "vips_success":
+                                    break;
+                                case "no_vips":
+                                    break;
+                                case "already_banned":
+                                case "bad_ban_admin":
+                                case "bad_ban_broadcaster":
+                                case "bad_ban_global_mod":
+                                case "bad_ban_self":
+                                case "bad_ban_staff":
+                                case "usage_ban":
+                                    break;
+                                case "ban_success":
+                                    break;
+                                case "usage_clear":
+                                    break;
+                                case "usage_mods":
+                                    break;
+                                case "mod_success":
+                                    break;
+                                case "usage_vips":
+                                    break;
+                                case "usage_vip":
+                                case "bad_vip_grantee_banned":
+                                case "bad_vip_grantee_already_vip":
+                                    break;
+                                case "vip_success":
+                                    break;
+                                case "usage_mod":
+                                case "bad_mod_banned":
+                                case "bad_mod_mod":
+                                    break;
+                                case "unmod_success":
+                                    break;
+                                case "unvip_success":
+                                    break;
+                                case "usage_unmod":
+                                case "bad_unmod_mod":
+                                    break;
+                                case "usage_unvip":
+                                case "bad_unvip_grantee_not_vip":
+                                    break;
+                                case "color_changed":
+                                    break;
+                                case "usage_color":
+                                case "turbo_only_color":
+                                    break;
+                                case "commercial_success":
+                                    break;
+                                case "usage_commercial":
+                                case "bad_commercial_error":
+                                    break;
+                                case "hosts_remaining":
+                                    break;
+                                case "bad_host_hosting":
+                                case "bad_host_rate_exceeded":
+                                case "bad_host_error":
+                                case "usage_host":
+                                    break;
+                                case "already_r9k_on":
+                                case "usage_r9k_on":
+                                    break;
+                                case "already_r9k_off":
+                                case "usage_r9k_off":
+                                    break;
+                                case "timeout_success":
+                                    break;
+                                case "delete_message_success":
+                                case "already_subs_off":
+                                case "usage_subs_off":
+                                    break;
+                                case "already_subs_on":
+                                case "usage_subs_on":
+                                    break;
+                                case "already_emote_only_off":
+                                case "usage_emote_only_off":
+                                    break;
+                                case "already_emote_only_on":
+                                case "usage_emote_only_on":
+                                    break;
+                                case "usage_slow_on":
+                                    break;
+                                case "usage_slow_off":
+                                    break;
+                                case "usage_timeout":
+                                case "bad_timeout_admin":
+                                case "bad_timeout_broadcaster":
+                                case "bad_timeout_duration":
+                                case "bad_timeout_global_mod":
+                                case "bad_timeout_self":
+                                case "bad_timeout_staff":
+                                    break;
+                                case "untimeout_success":
+                                case "unban_success":
+                                    break;
+                                case "usage_unban":
+                                case "bad_unban_no_ban":
+                                    break;
+                                case "usage_delete":
+                                case "bad_delete_message_error":
+                                case "bad_delete_message_broadcaster":
+                                case "bad_delete_message_mod":
+                                    break;
+                                case "usage_unhost":
+                                case "not_hosting":
+                                    break;
+                                case "whisper_invalid_login":
+                                case "whisper_invalid_self":
+                                case "whisper_limit_per_min":
+                                case "whisper_limit_per_sec":
+                                case "whisper_restricted_recipient":
+                                    break;
+                                case "no_permission":
+                                case "msg_banned":
+                                case "msg_room_not_found":
+                                case "msg_channel_suspended":
+                                case "tos_ban":
+                                    break;
+                                case "msg_rejected":
+                                case "msg_rejected_mandatory":
+                                    break;
+                                case "unrecognized_cmd":
+                                    break;
+                                case "cmds_available":
+                                case "host_target_went_offline":
+                                case "msg_censored_broadcaster":
+                                case "msg_duplicate":
+                                case "msg_emoteonly":
+                                case "msg_verified_email":
+                                case "msg_ratelimit":
+                                case "msg_subsonly":
+                                case "msg_timedout":
+                                case "msg_bad_characters":
+                                case "msg_channel_blocked":
+                                case "msg_facebook":
+                                case "msg_followersonly":
+                                case "msg_followersonly_followed":
+                                case "msg_followersonly_zero":
+                                case "msg_slowmode":
+                                case "msg_suspended":
+                                case "no_help":
+                                case "usage_disconnect":
+                                case "usage_help":
+                                case "usage_me":
+                                    break;
+                                case "host_on":
+                                case "host_off":
+                                    break;
+                                default:
+                                    break;
+                            }
+                            break;
+                        case 'USERNOTICE':
+                            switch ($message->id) {
+                                case "resub":
+                                    break;
+                                case "sub":
+                                    break;
+                                case "subgift":
+                                    break;
+                                case "anonsubgift":
+                                    break;
+                                case "submysterygift":
+                                    break;
+                                case "anonsubmysterygift":
+                                    break;
+                                case "primepaidupgrade":
+                                    break;
+                                case "giftpaidupgrade":
+                                    break;
+                                case "anongiftpaidupgrade":
+                                    break;
+                                case "raid":
+                                    break;
+                            }
+                            break;
+                        case 'HOSTTARGET':
+                            break;
+                        case 'CLEARCHAT':
+                            break;
+                        case 'CLEARMSG':
+                            break;
+                        case 'RECONNECT':
+                            break;
+                        case 'USERSTATE':
+                            break;
+                        case 'GLOBALUSERSTATE':
+                            break;
+                        case 'ROOMSTATE':
+                            break;
+                        case 'SERVERCHANGE':
+                            break;
+                        default:
+                            $this->logger->warning('Could not parse message: ' . $message->raw);
+                            break;
+                    }
+                } else if ($message->from === 'jtv') {
+                    switch ($message->type) {
+                        case 'MODE':
+                            break;
+                        default:
+                            $this->logger->warning('Could not parse message: ' . $message->raw);
+                            break;
+                    }
+                } else {
+                    switch ($message->type) {
+                        case '353':
+                            break;
+                        case '366':
+                            break;
+                        case 'JOIN':
+                            break;
+                        case 'PART':
+                            break;
+                        case 'WHISPER':
+                            break;
+                        case 'PRIVMSG':
+                            //FIXME put that treatment in a Command object
+                            if (substr($message->message, 0, $prefixlen) === $prefix) {
+                                $messagearr = explode(' ', $message->message);
+                                $message->command = substr(array_shift($messagearr), 1);
+                            }
+                            break;
+                        default:
+                            $this->logger->warning('Could not parse message: ' . $message->raw);
+                            break;
+                    }
+                }
             }
         }
     }
