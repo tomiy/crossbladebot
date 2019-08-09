8a5ff5646025cf602d87e849f3bc74c70fc0c5ad
tomiy <tom.chappaz1@gmail.com>
2019-08-09 12:28:43 +0200

    Cost display fix and better operator check

--- chat/channel.class.php
+++ chat/channel.class.php
@@ -11,7 +11,8 @@ class Channel extends RateLimit
     private $socket;
 
     public $name;
-    private $ismod;
+    private $isop;
+    private $modRequested;
 
     public function __construct($logger, $socket, Message $join)
     {
@@ -25,14 +26,21 @@ class Channel extends RateLimit
 
     public function userstate(Message $userstate)
     {
-        if ($userstate->tags['mod'] == true && !$this->ismod) {
+        if ($this->isOp($userstate) && !$this->isop) {
+            $this->isop = true;
             $this->logger->info('Setting rate limit to moderator settings');
             $this->setRate(100, 30);
-            $this->ismod = true;
-        } else if($userstate->tags['mod'] == false && $this->ismod) {
-            $this->logger->info('Setting rate limit to user settings');
-            $this->setRate(20, 30);
-            $this->ismod = false;
+        }
+        if (!$this->isOp($userstate)) {
+            if ($this->isop) {
+                $this->isop = false;
+                $this->logger->info('Setting rate limit to user settings');
+                $this->setRate(20, 30);
+            }
+            if (!$this->modRequested) {
+                $this->modRequested = true;
+                $this->send('Pssst, you should mod me so that i\'m able to send stuff with more ease!');
+            }
         }
     }
 
@@ -41,5 +49,17 @@ class Channel extends RateLimit
         $this->logger->info('Sending message: "' . trim($message) . '" to channel: ' . $this->name);
         $this->limit();
         $this->socket->send('PRIVMSG ' . $this->name . ' :' . $message . NL);
+    }
+
+    private function isOp($message) {
+        return $this->isMod($message) || $this->isBroadcaster($message);
+    }
+
+    private function isMod($message) {
+        return $message->tags['mod'];
+    }
+
+    private function isBroadcaster($message) {
+        return $message->tags['badges']['broadcaster'];
     }
 }
