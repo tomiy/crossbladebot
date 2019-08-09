b06d66440662942abbc6b90a24389f9ef5d2b608
tomiy <tom.chappaz1@gmail.com>
2019-08-09 16:55:28 +0200

    user level for commands

--- component/basic/channelmanager.class.php
+++ component/basic/channelmanager.class.php
@@ -8,13 +8,17 @@ class ChannelManager extends Component
 {
     public function join($message, $channel)
     {
-        $channel->sendRaw('JOIN #' . $message->user);
-        $channel->send('Joining channel #' . $message->user);
+        if ($channel->name === '#crossbladebot') { //TODO: make it a setting
+            $channel->sendRaw('JOIN #' . $message->user);
+            $channel->send('Joining channel #' . $message->user);
+        }
     }
-
+    
     public function part($message, $channel)
     {
-        $channel->sendRaw('PART #' . $message->user);
-        $channel->send('Leaving channel #' . $message->user);
+        if ($channel->name !== '#crossbladebot') { //TODO: make it a setting
+            $channel->sendRaw('PART #' . $message->user);
+            $channel->send('Leaving channel #' . $message->user);
+        }
     }
 }
