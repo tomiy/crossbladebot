bdf984a63b8c0643230ee0e24c7946d21a57f34f
tomiy <tom.chappaz1@gmail.com>
2019-08-14 13:27:51 +0200

    clean login notice and remove component channel fn

--- component/impl/helloworld.class.php
+++ component/impl/helloworld.class.php
@@ -12,11 +12,11 @@ class HelloWorld extends Component
     {
         $connectmsg = 'Connected to channel ' . $channel->getName();
         $this->logger->info($connectmsg);
-        return [$this->send($connectmsg, $channel)];
+        return [$channel->send($connectmsg)];
     }
 
     public function command(Message $message, Channel $channel): array
     {
-        return [$this->send('Command: ' . $message->getCommand(), $channel)];
+        return [$channel->send('Command: ' . $message->getCommand())];
     }
 }
